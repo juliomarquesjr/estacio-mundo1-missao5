9f0c18eeda1d3a761bf227b704285d8e2cf0dfb4
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-08-25 21:08:47 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import random
+from faker import Factory
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+fake = Factory.create('pt_BR')
+lista_palavras = []
+lista_pontos = []
+
+def numeros_em_texto(inteiro):
+    if inteiro == '1':
+        return "Um"
+    elif inteiro == '2':
+        return "Dois"
+    elif inteiro == '3':
+        return "Tres"
+    elif inteiro == '4':
+        return "Quatro"
+    elif inteiro == '5':
+        return "Cinco"
+    elif inteiro == '6':
+        return "Seis"
+    elif inteiro == '7':
+        return "Sete"
+    elif inteiro == '8':
+        return "Oito"
+    elif inteiro == '9':
+        return "Nove"
+    elif inteiro == '10':
+        return "Dez"
+
+### Cria lista com o nome das pessoas e a pontuação
+for _ in range(0, 30):
+    with open("lista_nomes.txt", "a", encoding="utf-8") as arquivo:
+        arquivo.write(fake.first_name() + ":" + str(random.randint(1, 10)) + '\n')
+
+### Abre o arquivo e monta a lista d
+with open("lista_nomes.txt", "r") as arquivo:
+    texto = arquivo.readlines()
+
+### Criando lista de palavras e dicionario com nomes
+for valores in texto:
+    temp = valores.replace("\n", "").split(":")
+    lista_palavras.append(numeros_em_texto(temp[1]))
+    lista_pontos.append(int(temp[1]))
+
+### Gerando imagem com Nuvem de palavras
+wordcloud = WordCloud().generate(str(lista_palavras).replace("'", ""))
+nuvem_palavras = plt
+nuvem_palavras.imshow(wordcloud, interpolation='bilinear')
+nuvem_palavras.axis("off")
+nuvem_palavras.show()
+
+### Gerando imagem com Nuvem de palavras
+histori = plt
+histori.hist(lista_pontos, bins=10)
+histori.show()
